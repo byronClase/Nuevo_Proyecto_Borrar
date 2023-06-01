c4789fd34efd85fe314abd49744c5865ae072fd3
byron.saul <yamahaaerox81@gmail.com>
2023-06-01 11:04:41 +0200

    deleted prints

--- app/src/main/java/nasimba/byron/gittutorial/MainActivity.java
+++ app/src/main/java/nasimba/byron/gittutorial/MainActivity.java
@@ -10,27 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");System.out.println("esto es un fiesta");
-        System.out.println("esto es un fiesta");
-        System.out.println("No es una prueba");
-        System.out.println("No es una prueba");
+
     }
 }
